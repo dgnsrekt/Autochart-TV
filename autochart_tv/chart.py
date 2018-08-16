9d20612ae5295bd2e73d608e5646c3af856e63a4
dgnsrekt <run2devtest@gmail.com>
2018-08-16 16:49:49 -0500

    renamed constants to options added studies.

--- autochart_tv/chart.py
+++ autochart_tv/chart.py
@@ -1,20 +1,34 @@
-from .constants import BARSTYLES, INTERVALS, TIMEZONES, THEMES
+from .options import BARSTYLES, INTERVALS, TIMEZONES, THEMES, STUDIES
 from .tools import validate
 
 
 class Chart:
-    
+
     def __init__(self,
                  symbol='BITFINEX:BTCUSD',
                  interval='1d',
                  timezone='UTC',
                  theme='Dark',
-                 barstyle='Candles'):
+                 barstyle='Candles',
+                 studies=['bb'],
+                 settings=None):
+
+        if settings:
+                interval =  settings['chart']['interval']
+                timezone =  settings['chart']['timezone']
+                theme =  settings['chart']['theme']
+                barstyle =  settings['chart']['barstyle']
+                studies =  settings['chart']['studies']
 
         validate(interval, INTERVALS, 'Interval')
         validate(timezone, TIMEZONES, 'Timezone')
         validate(theme, THEMES, 'Theme')
         validate(barstyle, BARSTYLES, 'Barstyle')
+
+        self.studies = list()
+        for study in studies:
+            validate(study, STUDIES, 'Study')
+            self.studies.append(STUDIES[study])
 
         self.symbol = symbol
         self.interval = INTERVALS[interval]
