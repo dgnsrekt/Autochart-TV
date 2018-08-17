da7b6d14a979fadd24e19462d20c4f6bed18bf3c
dgnsrekt <run2devtest@gmail.com>
2018-08-16 21:19:23 -0500

    started refactoring runclient

--- runclient.py
+++ runclient.py
@@ -2,55 +2,42 @@
 from collections import deque
 import json
 from pathlib import Path
+from random import choice, choices
 import time
 
 # thirdparty
-import iexfinance
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter # maynot need this
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import Completer, Completion
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.formatted_text import HTML
 
 from fuzzyfinder import fuzzyfinder
+import click
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
 import sys
-import pandas as pd
-import ccxt
+
+# local
+from autochart_tv.model import ChartModel, db
+from autochart_tv.exchange import ExchangeInterface
+
+
 
 #check 4 chromedriver also check if server is running
+
 print('loading exchange autocomoplete data...') #initallizing blah
-crypto_exchanges = ['binance', 'bittrex', 'poloniex']
-crypto = list()
-crypto_with_exchange = list()
-# notify which exchange is being downloaded symbols
-for exchange__ in crypto_exchanges:
-    try:
-        assert exchange__ in ccxt.exchanges
-        exchange = getattr(ccxt, exchange__)()
-        markets = exchange.load_markets()
-        coins = list(markets.keys())
-        coins = [coin.replace('/', '').upper() for coin in coins]
-        processsed = [f"{exchange__.upper()}:{coin}" for coin in coins]
 
-        crypto += coins
-        crypto_with_exchange += processsed
-        print('.', end='')
+active_symbol_interface = ExchangeInterface()
 
-    except AssertionError:
-        raise Exception(f'{exchange} exchange doesnt exist.')
+commands = ['EXIT', 'QUIT', 'DELETE', 'CLEAR', 'RANDOM', 'RANDOM_CRYPTO']
 
-df = pd.DataFrame(iexfinance.get_available_symbols())
-stocks = list(df[df.isEnabled == True]['symbol'])
-print('.')
-print('loaded')
+# words = stocks + list(set(crypto)) + crypto_with_exchange + commands
 
-commands = ['EXIT', 'QUIT', 'DELETE', 'RANDOM', 'CLEAR', 'FULL_RANDOM', 'FULL_RANDOM_CRYPTO']
-crypto = list(set(crypto))
-words = stocks + list(set(crypto)) + crypto_with_exchange + commands
 url = 'http://localhost:5000'
 
 class AutoChartCompleter(Completer):
@@ -60,26 +47,16 @@ class AutoChartCompleter(Completer):
         for m in matches:
             yield Completion(m, start_position=-len(word_before_cursor))
 
-tv_wordcompleter = WordCompleter(words, ignore_case=True)
-
 class MyChrome(webdriver.Chrome):
     def quit(self):
         webdriver.Chrome.quit(self)
         self.session_id = None
 
 
-from autochart_tv.model import ChartModel, db
-import sys
-from random import choice, choices
-
 driver = MyChrome()
 driver.get(url)  # tested in combination with scrapy
 
 # KEY BINDINGS
-from prompt_toolkit.application import run_in_terminal
-from prompt_toolkit.key_binding import KeyBindings
-import click
-
 bindings = KeyBindings()
 
 @bindings.add('c-z')
@@ -89,15 +66,13 @@ def _(event):
         driver.refresh()
     run_in_terminal(print_hello)
 
-from prompt_toolkit import prompt
-from prompt_toolkit.formatted_text import HTML
 
 def bottom_toolbar():
     return HTML('This is a <b><style bg="ansired">Toolbar</style></b>!')
 
 class InvalidEntryError(Exception):
     pass
-    
+
 while True:
     inp = prompt('> ',
                  completer=AutoChartCompleter(), complete_while_typing=True, history=FileHistory('history.txt'), key_bindings=bindings, bottom_toolbar=bottom_toolbar)
