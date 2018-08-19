5d0ef4a6f52d2ab60e674586e4364fae6dd71391
dgnsrekt <run2devtest@gmail.com>
2018-08-19 09:09:40 -0500

    refactored exampl3 added 4

--- exampl4.py
+++ exampl4.py
@@ -6,21 +6,21 @@ from collections import OrderedDict
 import sys
 import re
 
-# TWITTER_NAMES = ['SeekingAlpha', 'StockTexts', 'JrwlkrT']
-TWITTER_NAMES = ['SeekingAlpha']
+TWITTER_NAMES = ['AT09_Trader']
 
 def get_stocks_from_twitter(twitter_name):
     first_ticker_from_tweet = list()
-    tickerTweetRegex = re.compile(r'\$\D\w*')
-    for tweet in get_tweets(twitter_name, pages=1):
+    tickerTweetRegex = re.compile(r'\$[^\d\s]\w*')
+    for tweet in get_tweets(twitter_name, pages=5): # should be a generator somewhere.
         mo = tickerTweetRegex.search(tweet['text'])
         if mo is not None:
             first_ticker_from_tweet.append(mo.group())
     clean = [ticker.replace('$', '') for ticker in first_ticker_from_tweet]
     clean = list(OrderedDict.fromkeys(clean))
-    print(clean)
     return clean[:9]
 
+# get_stocks_from_twitter(TWITTER_NAMES[0])
+#
 with ACManager() as ACM:
     last = ''
     while True:
