0f9e459f3c637670c2595b76bcce018af7f7c45d
dgnsrekt <run2devtest@gmail.com>
2018-08-21 10:21:42 -0500

    updated example 4

--- examples/exampl4.py
+++ examples/exampl4.py
@@ -14,37 +14,34 @@ from collections import OrderedDict
 import sys
 import re
 
-TWITTER_NAMES = ['AT09_Trader']
+import pandas as pd
 
-def get_stocks_from_twitter(twitter_name):
-    first_ticker_from_tweet = list()
+TWITTER_NAMES = 'InvestorsLive'
+
+def get_stocks_from_twitter(twitter_name, max_tickers=9):
     tickerTweetRegex = re.compile(r'\$[^\d\s]\w*')
+    tickers = list()
     for tweet in get_tweets(twitter_name, pages=5): # should be a generator somewhere.
-        mo = tickerTweetRegex.search(tweet['text'])
-        if mo is not None:
-            first_ticker_from_tweet.append(mo.group())
-    clean = [ticker.replace('$', '') for ticker in first_ticker_from_tweet]
-    clean = list(OrderedDict.fromkeys(clean))
-    return clean[:9]
+        mo = tickerTweetRegex.findall(tweet['text'])
+        if len(mo) > 0:
+            clean = [ticker.replace('$', '') for ticker in mo]
+            tickers += clean
 
-# get_stocks_from_twitter(TWITTER_NAMES[0])
-#
-with ACManager() as ACM:
-    last = ''
+    results = pd.unique(tickers)
+
+    return list(results[:9])
+
+with ACManager() as ACM: #TODO add var to change the title
+    last = []
     while True:
-        twitter = choice(TWITTER_NAMES)
+        twitter = TWITTER_NAMES
         print(f'Pulling nine tickers from {twitter} tweets')
-        loops = 0
         while True:
             stocks = get_stocks_from_twitter(twitter)
             if last != stocks:
                 last = stocks
+                print()
                 print(f'New stock found {stocks[0]}')
-            # print(stocks)
             ACM['CHART'].execute(*stocks)
             sleep(5)
-            loops +=1
-            print('.', end='')
-            if loops > 10:
-                print()
-                break
+            print('.', end='', flush=True)
